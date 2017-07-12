a41c63ab85aa8ed19b212c1d2233ba3c9343da40
T-G <pronoygomes@yahoo.com>
2017-07-12 12:52:19 +0100

    add angularjs. bootstrap3

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("myController", [ "$scope", function($scope){
+    $scope.users = [
+        {username:"pronoy", fullName: "Theotones Gomes", email: "pronoygomes@yahoo.com"},
+        {username:"don", fullName: "Donney Jones", email: "don@yahoo.com"},
+        {username:"lola", fullName: "Lola Maps", email: "lola@yahoo.com"},
+    ];
+
+// initialize new user empty object
+$scope.newUser = {};
+$scope.selectedUser = {};
+
+// Add User
+$scope.saveUser = function(user){
+    $scope.users.push($scope.newUser);
+    $scope.newUser = {};
+};
+
+// Edit User
+$scope.editUser = function(user){
+    $scope.selectedUser = $scope
+};
+
+// Delete User
+
+}]);
+
