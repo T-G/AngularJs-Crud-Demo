3222bdeb6b943510abc56bd43564161d6155906c
T-G <pronoygomes@yahoo.com>
2017-07-12 18:38:16 +0100

    add validation to create user form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("myController", [ "$scope", function($scope){
+myApp.controller("myController", [ "$scope","utilityService", function($scope, utilityService){
     $scope.users = [
-        {username:"pronoy", fullName: "Theotones Gomes", email: "pronoygomes@yahoo.com"},
+        {username:"janedoe", fullName: "Jane Doe", email: "jane@test.com"},
         {username:"don", fullName: "Donney Jones", email: "don@yahoo.com"},
         {username:"lola", fullName: "Lola Maps", email: "lola@yahoo.com"},
     ];
@@ -12,10 +12,15 @@ $scope.newUser = {};
 $scope.selectedUser = {};
 $scope.message = "";
 // Add User
-$scope.saveUser = function(user){
-    $scope.users.push($scope.newUser);
-    $scope.newUser = {};
-    $scope.message = "New User added successfully.";
+$scope.saveUser = function(user, IsValid){
+    if(IsValid){
+        $scope.users.push($scope.newUser);
+        $scope.newUser = {};
+        $scope.message = "New User added successfully.";
+        $scope.Flg = true;
+        utilityService.myAlert();
+    }
+    
 };
 
 // Edit User
@@ -25,12 +30,16 @@ $scope.selectUser = function(user){
 
 $scope.updateUser = function(){
     $scope.message = "User Updated successfull.";
+    $scope.Flg = true;
+    utilityService.myAlert();
 };
 
 // Delete User
 $scope.deleteUser = function(){
     $scope.users.splice($scope.users.indexOf($scope.selectedUser), 1);
     $scope.message = "User deleted successfully.";
+    $scope.Flg = true;
+    utilityService.myAlert();
 };
 
 $scope.clearMessage = function(){
@@ -40,3 +49,17 @@ $scope.clearMessage = function(){
 
 }]);
 
+
+// Global Services
+myApp.factory("utilityService", [function () {
+    utilityObj = {};
+
+    // Success message animation
+    utilityObj.myAlert = function () {
+        $("#alert").fadeTo(2000, 500).slideUp(1000, function () {
+            $('#alert').slideUp(1000);
+        });
+    }
+
+    return utilityObj;
+}]);
